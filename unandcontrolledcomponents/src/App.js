01e29f3f5d4fdc531694dc2a1d34338061a8c01c
hugo.zepeda <A01228679@exatec.tec.mx>
2023-02-15 15:19:22 -0600

    Controlled Onboarding Flow

--- unandcontrolledcomponents/src/App.js
+++ unandcontrolledcomponents/src/App.js
@@ -1,25 +1,53 @@
 import { useState } from 'react';
-import { UncontrolledForm } from './UncontrolledForm';
-import { ControlledForm } from './ControlledForm';
-import { UncontrolledModal } from './UncontrolledModal';
-import { ControlledModal } from './ControlledModal';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+
+const StepOne = ({ goToNext }) => (
+    <>
+        <h1>Step 1</h1>
+        <button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+    </>
+);
+const StepTwo = ({ goToNext }) => (
+    <>
+        <h1>Step 2</h1>
+        <button onClick={() => goToNext({ age: 50 })}>Next</button>
+    </>
+);
+const StepThree = ({ goToNext }) => (
+    <>
+        <h1>Step 3</h1>
+        <p>Congratulations! You qualify for our senior discount</p>
+        <button onClick={() => goToNext({})}>Next</button>
+    </>
+);
+const StepFour = ({ goToNext }) => (
+    <>
+        <h1>Step 4</h1>
+        <button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+    </>
+);
 
 function App() {
-  const [shouldShowModal, setShouldShowModal] = useState(false);
+    const [onboardingData, setOnboardingData] = useState({});
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  return (
-      <>
-        <ControlledModal
-            shouldShow={shouldShowModal}
-            onRequestClose={() => setShouldShowModal(false)}
+    const onNext = stepData => {
+        setOnboardingData({ ...onboardingData, ...stepData });
+        setCurrentIndex(currentIndex + 1);
+    }
+
+    return (
+        <ControlledOnboardingFlow
+            currentIndex={currentIndex}
+            onNext={onNext}
         >
-          <h1>Hello!</h1>
-        </ControlledModal>
-        <button onClick={() => setShouldShowModal(!shouldShowModal)}>
-          {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
-        </button>
-      </>
-  );
+            <StepOne />
+            <StepTwo />
+            {onboardingData.age >= 62 && <StepThree />}
+            <StepFour />
+        </ControlledOnboardingFlow>
+    );
 }
 
 export default App;
