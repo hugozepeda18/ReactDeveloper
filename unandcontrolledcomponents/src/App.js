f30cd6ed4d2205aa9c45c8fbf83cac608fad1598
hugo.zepeda <A01228679@exatec.tec.mx>
2023-02-15 15:18:06 -0600

    Controlled Modal

--- unandcontrolledcomponents/src/App.js
+++ unandcontrolledcomponents/src/App.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import { UncontrolledForm } from './UncontrolledForm';
+import { ControlledForm } from './ControlledForm';
+import { UncontrolledModal } from './UncontrolledModal';
+import { ControlledModal } from './ControlledModal';
 
 function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
+
   return (
-      <UncontrolledForm />
+      <>
+        <ControlledModal
+            shouldShow={shouldShowModal}
+            onRequestClose={() => setShouldShowModal(false)}
+        >
+          <h1>Hello!</h1>
+        </ControlledModal>
+        <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+          {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+        </button>
+      </>
   );
 }
 
